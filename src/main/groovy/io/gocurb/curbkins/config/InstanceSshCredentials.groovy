ee6a9657b78890ff9aad48774962996381e35a8b
Stephen Garlick <sgarlick@taximagic.com>
2015-05-11 16:08:00 -0400

    more test

--- src/main/groovy/io/gocurb/curbkins/config/InstanceSshCredentials.groovy
+++ src/main/groovy/io/gocurb/curbkins/config/InstanceSshCredentials.groovy
@@ -1,5 +1,6 @@
 package io.gocurb.curbkins.config
 
+import com.cloudbees.jenkins.Credential
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey
 import com.cloudbees.plugins.credentials.CredentialsScope
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider
@@ -9,13 +10,17 @@ import com.cloudbees.plugins.credentials.SystemCredentialsProvider
  */
 class InstanceSshCredentials implements InstanceConfig {
     SystemCredentialsProvider systemCredentialsProvider
+    Credential credential
     def configure() {
         def creds = systemCredentialsProvider.getCredentials()
         if(creds.size() == 0) {
-            def keySource = new BasicSSHUserPrivateKey.UsersPrivateKeySource()
-            def cred = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL, 'curbkins-github', 'curbkins-github', keySource, '', '')
-            creds.add(cred)
+            creds.add(credential)
+            systemCredentialsProvider.save()
         }
-        systemCredentialsProvider.save()
+    }
+
+    static def getCredentials() {
+        def privateKeySource = new BasicSSHUserPrivateKey.UsersPrivateKeySource()
+        return new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL, 'curbkins-github', 'curbkins-github', privateKeySource, '', '')
     }
 }
--- src/test/groovy/io/gocurb/curbkins/config/IntanceSshCredentialsSpecification.groovy
+++ src/test/groovy/io/gocurb/curbkins/config/IntanceSshCredentialsSpecification.groovy
@@ -1,10 +1,32 @@
 package io.gocurb.curbkins.config
 
+import com.cloudbees.jenkins.Credential
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider
+import com.google.common.collect.Lists
 import spock.lang.Specification
-
 /**
  * Created by sgarlick on 5/11/15.
  */
 class IntanceSshCredentialsSpecification extends Specification {
+    SystemCredentialsProvider systemCredentialsProvider
+    InstanceSshCredentials instanceSshCredentials
+    Credential credential
+    def setup() {
+        systemCredentialsProvider = Mock(SystemCredentialsProvider)
+        systemCredentialsProvider.getCredentials() >> Lists.newArrayList()
+        credential = Mock(Credential)
+        instanceSshCredentials = new InstanceSshCredentials(systemCredentialsProvider: systemCredentialsProvider,
+                                                            credential: credential)
+    }
 
+    def "the credential is saved"() {
+        when:
+        instanceSshCredentials.configure()
+
+        then:
+        def creds = systemCredentialsProvider.getCredentials()
+        creds.size() == 1
+        creds.iterator().next() == credential
+        systemCredentialsProvider.save()
+    }
 }
