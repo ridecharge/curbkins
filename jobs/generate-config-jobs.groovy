4139080dd9c8db51542c7d59a09407a43033ef82
Stephen Garlick <sgarlick@taximagic.com>
2015-05-12 11:32:48 -0400

    make file aval during test

--- jobs/generate-config-jobs.groovy
+++ jobs/generate-config-jobs.groovy
@@ -2,4 +2,10 @@ import io.gocurb.curbkins.config.InstanceConfigJobs
 import javaposse.jobdsl.dsl.DslFactory
 
 def dslFactory = this as DslFactory
+
+dslFactory.job('generate-config-jobs') {
+    for(jobEntry in InstanceConfigJobs.configJobs) {
+
+    }
+}
 jobs = InstanceConfigJobs.get(dslFactory).configure()
--- src/test/groovy/io/gocurb/curbkins/jobs/JobScriptSpec.groovy
+++ src/test/groovy/io/gocurb/curbkins/jobs/JobScriptSpec.groovy
@@ -1,11 +1,11 @@
 
+
 import groovy.io.FileType
 import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.dsl.JobManagement
 import javaposse.jobdsl.dsl.MemoryJobManagement
 import spock.lang.Specification
 import spock.lang.Unroll
-
 /**
  * Tests that all dsl scripts in the jobs directory will compile.
  * Taken from https://github.com/sheehan/job-dsl-gradle-example
@@ -15,7 +15,9 @@ class JobScriptsSpec extends Specification {
     @Unroll
     void 'test script #file.name'(File file) {
         given:
+
         JobManagement jm = new MemoryJobManagement()
+        jm.availableFiles['jobs/generate-config-jobs.groovy'] = 'hello'
 
         when:
         DslScriptLoader.runDslEngine file.text, jm
