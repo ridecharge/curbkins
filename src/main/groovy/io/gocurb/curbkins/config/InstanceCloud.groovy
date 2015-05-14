88845f8713167e8f659f6034b7e2ec949ee8bb7f
Stephen Garlick <sgarlick@taximagic.com>
2015-05-14 12:29:31 -0400

    start ec2 work

---
+++ src/main/groovy/io/gocurb/curbkins/config/InstanceCloud.groovy
@@ -0,0 +1,13 @@
+package io.gocurb.curbkins.config
+
+import jenkins.model.Jenkins
+
+/**
+ * Created by sgarlick on 5/14/15.
+ */
+class InstanceCloud {
+    Jenkins jenkins
+
+    def configure() {
+    }
+}
