842f1ceb46986048ec169aa272327b06dccafc4b
Stephen Garlick <sgarlick@taximagic.com>
2015-05-14 13:26:23 -0400

    slave templates next

--- src/main/groovy/io/gocurb/curbkins/config/InstanceCloud.groovy
+++ src/main/groovy/io/gocurb/curbkins/config/InstanceCloud.groovy
@@ -1,13 +1,28 @@
 package io.gocurb.curbkins.config
 
+import hudson.plugins.ec2.AmazonEC2Cloud
+import hudson.plugins.ec2.SlaveTemplate
+import hudson.slaves.Cloud
 import jenkins.model.Jenkins
 
 /**
  * Created by sgarlick on 5/14/15.
  */
 class InstanceCloud {
+
     Jenkins jenkins
+    Cloud cloud
 
     def configure() {
+        jenkins.clouds.add(cloud)
+        jenkins.save()
+    }
+
+    static def get() {
+        return new InstanceCloud(jenkins: Jenkins.instance, cloud: getAmazonEC2Cloud())
+    }
+    static def getAmazonEC2Cloud() {
+        SlaveTemplate template = new SlaveTemplate()
+        return new AmazonEC2Cloud(true, '', '', 'us-east-1', '', '3', null);
     }
 }
